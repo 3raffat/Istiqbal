1f4b625dac5e1b94c30f9a765208510377222715
3raffat <mhmd3raffat@gmail.com>
2025-10-15 20:17:26 +0300

    Initial template

---
+++ FrontEnd/src/App.tsx
@@ -0,0 +1,14 @@
+import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './Routes'
+
+function App() {
+
+  return (
+    <>
+     <RouterProvider router={router}/>
+    </>
+  )
+}
+
+export default App
